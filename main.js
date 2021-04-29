0cf93f57a445b8d4e03807a3eabcf37e313e4e28
shloaky <77309360+shloaky@users.noreply.github.com>
2021-04-29 12:07:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+student_array = [];
+function submit(){
+    var name_1 = document.getElementById("student1").value;
+    var name_2 = document.getElementById("student2").value;
+    var name_3 = document.getElementById("student3").value;
+    var name_4 = document.getElementById("student4").value;
+
+    student_array.push(name_1);
+    student_array.push(name_2);
+    student_array.push(name_3);
+    student_array.push(name_4);
+
+    console.log(student_array);
+
+    document.getElementById("display_name").innerHTML=student_array;
+    document.getElementById("submit_button").style.display="none";
+    document.getElementById("sort_button").style.display="inline-block"; 
+}
+function sort(){
+    student_array.sort();
+    console.log(student_array);
+    document.getElementById("display_name").innerHTML=student_array;
